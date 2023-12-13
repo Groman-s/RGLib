8f2ceabc534716002282c2c93ee7ad8462f3d7d3
Groman-S <goyanov99@mail.ru>
2023-12-13 17:18:47 +0300

    Добавил функции moveEntityInDirectionOfEntity, forceEntityLookAtEntity, getLocationInFrontOfEntity, getNearbyBlocks.

--- src/main/java/com/goyanov/rglib/RGAPI.java
+++ src/main/java/com/goyanov/rglib/RGAPI.java
@@ -5,10 +5,14 @@ import net.md_5.bungee.api.chat.ComponentBuilder;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Sound;
+import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.util.Vector;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class RGAPI
 {
@@ -77,6 +81,54 @@ public class RGAPI
         if (who.add(atWho).length() < delta) return true;
 
         return false;
+    }
+
+    public static void moveEntityInDirectionOfEntity(Entity from, Entity to, double speed)// ДВИГАЕМ СУЩНОСТЬ В СТОРОНУ ДРУГОЙ СУЩНОСТИ С ЗАДАННОЙ СКОРОСТЬЮ
+    {
+        Location fromL = from.getLocation();
+        Location toL = to.getLocation();
+
+        Vector direction = new Vector(toL.getX() - fromL.getX(), toL.getY() - fromL.getY(), toL.getZ() - fromL.getZ());
+        if (direction.length() != 0) direction = direction.normalize().multiply(speed);
+        fromL.add(direction);
+        from.teleport(fromL);
+    }
+
+    public static void forceEntityLookAtEntity(Entity who, Entity atWho)
+    {
+        Location from = who.getLocation();
+        Location to = atWho.getLocation();
+
+        Vector direction = new Vector(to.getX() - from.getX(), to.getY() - from.getY(), to.getZ() - from.getZ());
+        from.setDirection(direction);
+
+        who.teleport(from);
+    }
+
+    public static Location getLocationInFrontOfEntity(Entity ent, float distance)// ПОЛУЧАЕМ ЛОКАЦИЮ ПРЯМО ПЕРЕД (ЗА) СУЩНОСТЬЮ В НЕСКОЛЬКИХ БЛОКАХ
+    {
+        Location pLoc = ent.getLocation();
+        pLoc.setPitch(0);
+        Vector viewLoc = pLoc.getDirection().normalize().multiply(distance);
+        pLoc = pLoc.add(viewLoc.getX(), 0, viewLoc.getZ());
+        if (distance > 0) pLoc.setYaw(pLoc.getYaw()-180);
+        return pLoc;
+    }
+
+    public static List<Block> getNearbyBlocks(Player p, int x, int y, int z)
+    {
+        List<Block> list = new ArrayList<>();
+        Location loc = p.getLocation();
+        World w = loc.getWorld();
+
+        for (int i = -x; i <= x; i++)
+            for (int j = -y; j <= y; j++)
+                for (int k = -z; k <= z; k++)
+                {
+                    list.add(w.getBlockAt(loc.clone().add(i,j,k)));
+                }
+
+        return list;
     }
 
     public static Location getTheNearestEmptyLocation(Location loc)
